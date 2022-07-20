5707076fcc72e01772ec01629a3fd252861697e3
ngocphm <minhngoc1122000@gmail.com>
2022-07-21 00:34:49 +0700

    update2

--- QuanLy/app/src/main/java/com/example/quanly/fragment/sanpham_fragment.java
+++ QuanLy/app/src/main/java/com/example/quanly/fragment/sanpham_fragment.java
@@ -1,20 +1,60 @@
 package com.example.quanly.fragment;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.quanly.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class sanpham_fragment extends Fragment {
+    private FloatingActionButton fab ;
+    private View view ;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sanpham,container,false);
+        view = inflater.inflate(R.layout.fragment_sanpham,container,false);
+        FloatingActionButton fab = view.findViewById(R.id.fab_sanPham);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                themSanPham(Gravity.CENTER);
+            }
+        });
+        return view;
+    }
+    private void themSanPham(int gravity){
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_them_sanpham);
+
+        Window window = dialog.getWindow();
+        if(window==null){
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAtributes = window.getAttributes();
+        windowAtributes.gravity = gravity ;
+        window.setAttributes(windowAtributes);
+        if(Gravity.CENTER == gravity){
+            dialog.setCancelable(true);
+        } else {
+            dialog.setCancelable(false);
+        }
+        dialog.show();
     }
 }
--- QuanLy/app/src/main/java/com/example/quanly/fragment/taikhoan_fragment.java
+++ QuanLy/app/src/main/java/com/example/quanly/fragment/taikhoan_fragment.java
@@ -1,21 +1,60 @@
 package com.example.quanly.fragment;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.quanly.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class taikhoan_fragment extends Fragment {
-
+    private FloatingActionButton fab ;
+    private View view ;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_taikhoan,container,false);
+         view = inflater.inflate(R.layout.fragment_taikhoan,container,false);
+        FloatingActionButton fab = view.findViewById(R.id.fab_taiKhoan);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                themTaiKhoan(Gravity.CENTER);
+            }
+        });
+        return view ;
+    }
+    private void themTaiKhoan(int gravity){
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_them_taikhoan);
+
+        Window window = dialog.getWindow();
+        if(window==null){
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAtributes = window.getAttributes();
+        windowAtributes.gravity = gravity ;
+        window.setAttributes(windowAtributes);
+        if(Gravity.CENTER == gravity){
+            dialog.setCancelable(true);
+        } else {
+            dialog.setCancelable(false);
+        }
+        dialog.show();
     }
 }
